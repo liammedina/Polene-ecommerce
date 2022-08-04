67f4049d8e83933eafdca8d0fbf8c7b222619e18
Lia M. Medina <54250767+Niugler@users.noreply.github.com>
2022-08-04 19:45:23 -0300

    Desafío 4

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Title from "../title"
+import ItemCount from "../ItemCount";
 
 export const ItemListContainer = ({texto}) => {
+
+   const onAdd = (quantity) =>{
+    console.log(`Compraste ${quantity} unidades`)
+   }
+
+
     return ( 
-        <Title greeting= {texto}/>
-     );
+        <>
+            <Title greeting= {texto}/>
+            <ItemCount initial={1} stock={5} onAdd={onAdd}/>
+        </>
+        );
 }
  
 export default ItemListContainer;
