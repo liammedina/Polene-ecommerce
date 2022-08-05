e6ddd3b138c17133424f9ed4bda1849ec4fcdd63
Lia M. Medina <54250767+Niugler@users.noreply.github.com>
2022-08-04 23:06:56 -0300

    Desafío 7

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,10 +1,11 @@
 import "./Item.css";
 import React from "react";
+import {Link} from 'react-router-dom'
 
 const Item = ({info}) => {
     
     return ( 
-        <a href="" className="card" style={{width: "17rem"}}>
+        <Link to="/detalle" href="" className="card" style={{width: "17rem"}}>
             <img className="card-img-top" src={info.img} alt="" />
             <div  className="card-body d-flex flex-column justify-content-center"></div>
                 <h4  className="card-title">{info.title}</h4>
@@ -12,7 +13,7 @@ const Item = ({info}) => {
                     <h4>${info.price}</h4>
                     <button className="btn btn-primary">Ver Detalle del Producto</button>
                 </div>
-        </a>
+        </Link>
      );
 }
  
